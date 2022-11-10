7685c8ff6c9c89fcc0548c07ffa3b8cb64b69b33
JoaoGMagri <jgmagri@hotmail.com>
2022-11-10 01:51:20 -0300

    Post e Get criados

--- index.js
+++ index.js
@@ -21,20 +21,21 @@ app.post("/participants", async (req, res) => {
         return res.status(422).send("Insira um nome do usuario");
     }
 
-
     try {
-        const nameUsers = await collectionUsers.find({}).toArray();
 
-        if (nameUsers.find(obj => obj.name === name)) {
+        const nameUsers = await collectionUsers.find().toArray();
+        const teste = nameUsers.find(obj => obj.name === name)
+
+        if (teste) {
             return res.status(409).send("Nome do usuario já existe");
         }
 
-        db.collection("users").insertOne({
+        await db.collection("users").insertOne({
             name,
             lastStatus: Date.now()
         });
 
-        db.collection("message").insertOne({
+        await db.collection("message").insertOne({
             from: name,
             to: 'Todos',
             text: 'entra na sala...',
@@ -42,22 +43,69 @@ app.post("/participants", async (req, res) => {
             time: dayjs().format("HH:mm:ss"),
         })
 
-        res.status(201)
+        res.sendStatus(201);
+
+    } catch (err) {
+        console.log("Erro com o servidor");
+    }
+
+});
+
+app.get("/participants", async (req, res) => {
+
+    try {
+        const user = await collectionUsers.find().toArray();
+        res.status(201).send(user);
+    } catch (err) {
+        console.log("Erro participant" + err);
+    }
+
+});
+
+app.post("/messages", async (req, res) => {
+
+    const { to, text, type } = req.body;
+    const { user } = req.headers;
+    try {
+
+        /* const nameUsers = await collectionUsers.find({}).toArray();
+
+        if ( !to ){
+            return res.status(422).send("Campo 'to' invalido");
+        } else if( !text ) {
+            return res.status(422).send("Campo 'text' invalido");
+        } else if( !(type === "message") || !(type !== "private_message") ) {
+            return res.status(422).send("Campo 'type' invalido");
+        } else if( nameUsers.find(obj => obj.name === user)) {
+            return res.status(422).send("Usuario não logado");
+        } */
+
+        await db.collection("message").insertOne({
+            from: user,
+            to,
+            text,
+            type,
+            time: dayjs().format("HH:mm:ss"),
+        });
+
+        res.sendStatus(201);
 
     } catch (err) {
         res.send("Erro com o servidor");
     }
 
-})
+});
 
-app.get("/participants", async (req, res) => {
+app.get("/messages", async (req, res) => {
 
-    const nameUsers = await collectionUsers.find({}).toArray();
+    try {
+        const messageArray = await collectionMessage.find().toArray();
+        res.status(201).send(messageArray);
+    } catch (err) {
+        console.log("Erro message" + err);
+    }
 
-    res.send(nameUsers);
-
-})
-
+});
 
 app.listen(5000, () => {
     console.log("Server running in port: 5000")
